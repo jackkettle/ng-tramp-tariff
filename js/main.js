14fdeea875bbdc6258dea564859909e0d7952a99
cammelCase <jack.kettle2@mail.dcu.ie>
2014-11-24 00:10:32 +0000

    Messing about

--- js/main.js
+++ js/main.js
@@ -7,7 +7,35 @@ angular
 
         $http.get('json/tariff_skills.json')
             .then(function(res){
+
                 $scope.moves = res.data;
+                $scope.novice = [];
+                $scope.intermediate = [];
+                $scope.advanced = [];
+                $scope.elite = [];
+
+                $scope.moves.forEach(function (move) {
+
+                    if(move.level == 'Elite'){
+                        //console.log("Elite: " + move.skill);
+                        $scope.elite.push(move);
+                    }
+                    else if(move.level == 'Advanced') {
+                        //console.log("Advanced: " + move.skill);
+                        $scope.advanced.push(move);
+                    }
+                    else if(move.level == 'Intermediate') {
+                        //console.log("Intermediate: " + move.skill);
+                        $scope.intermediate.push(move);
+                    }
+                    else if(move.level == 'Novice') {
+                        //console.log("Novice: " + move.skill);
+                        $scope.novice.push(move);
+                    }
+
+
+                });
+
             });
 
         $scope.parseHtml = function($scope) {
