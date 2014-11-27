d513c029346d643582e5e06420e1074dab3ea408
Jack Kettle <jack.kettle2@mail.dcu.ie>
2014-11-27 16:16:45 +0000

    added in initial version of shape addition, needs work

--- js/main.js
+++ js/main.js
@@ -9,49 +9,64 @@ angular
             .then(function(res){
 
                 $scope.moves = res.data;
-
+                $scope.Math = window.Math;
                 $scope.userRoutine = [1,2,3,4,5,6,7,8,9,10];
-                var sum = 0;
-                for (var i = 0 ; i < $scope.userRoutine.length; i++) {
-                    if($scope.userRoutine[i].tariff){
-                        sum += $scope.userRoutine[i].tariff;
-                    }
-                }
-                $scope.sum = sum;
-                
-                $scope.onChange = function () {
-  
+                 
+                $scope.onChangeSelect = function () {
+                    console.log("------------On change------------");
+                    
                     // check for repeat skills
                     for (var i = $scope.userRoutine.length - 1; i >= 1; i--) {
                         // check if skill is set
                         if($scope.userRoutine[i].skill){
                             // check moves below move i
-                            for (var j = i ; j >= 0; j--) {
+                            for (var j = i - 1; j >= 0; j--) {
                                 // if its set and is the same as the move i
                                 if($scope.userRoutine[j].skill && 
-                                $scope.userRoutine[i].skill == $scope.userRoutine[j].skill){
+                                $scope.userRoutine[i].skill === $scope.userRoutine[j].skill){
                                     // copy new move and set tariff to 0
                                     var newMove =  angular.copy($scope.userRoutine[i]);
                                     newMove.tariff = 0;
-                                    $scope.userRoutine[i]= newMove;
                                     // mark move as a repeated move
-                                    $scope.userRoutine[i].repeatMove = true;
-
+                                    newMove.repeatMove = true;
+                                    $scope.userRoutine[i] = newMove;
                                 }
                             }
                         }
                     }
-                    checkTariff($scope.userRoutine);
+                    
+                    // update total tariff
+                    $scope.sum = checkTariff($scope.userRoutine);
                 }
                 
+                $scope.checkMoveTariff = function (move) {
+                    var sum = 0.0;
+                    if(move.tariff && move.shapeTariff){
+                        sum += move.tariff + move.shapeTariff;
+                        
+                    }else if(move.tariff){
+                        sum += move.tariff;
+                    }
+                    return Math.round( sum * 10) / 10;
+                }
+                
+                function checkMoveTariff (move) {
+                    var sum = 0.0;
+                    if(move.tariff && move.shapeTariff){
+                        sum += move.tariff + move.shapeTariff;
+                        
+                    }else if(move.tariff){
+                        sum += move.tariff;
+                    }
+                    return Math.round( sum * 10) / 10;
+                }
+
                 function checkTariff (userRoutine) {
                     var sum = 0.0;
-                    for (var i = 0 ; i < userRoutine.length; i++) {
-                        if(userRoutine[i].tariff){
-                            sum += userRoutine[i].tariff;
-                        }
+                    for(var i = userRoutine.length - 1; i >= 0; i--){
+                        sum += checkMoveTariff(userRoutine[i]);
                     }
-                    $scope.sum = Math.round( sum * 10) / 10;
+                    return Math.round( sum * 10) / 10;
                 }
                 
             });
