7d0235e149fc3a81e32f9e4aa01c46da3628be63
Jack Kettle <jack.kettle2@mail.dcu.ie>
2014-11-28 11:37:59 +0000

    Pretty much finshed barbones version. Need to refactor code. Maybe consider adding in routines and warnings for moves start/finish position

--- js/main.js
+++ js/main.js
@@ -17,19 +17,31 @@ angular
                     
                     // check for repeat skills
                     for (var i = $scope.userRoutine.length - 1; i >= 1; i--) {
+                        
                         // check if skill is set
                         if($scope.userRoutine[i].skill){
+                            $scope.userRoutine[i].repeatMove = false;
                             // check moves below move i
                             for (var j = i - 1; j >= 0; j--) {
                                 // if its set and is the same as the move i
                                 if($scope.userRoutine[j].skill && 
-                                $scope.userRoutine[i].skill === $scope.userRoutine[j].skill){
+                                $scope.userRoutine[i].skill === $scope.userRoutine[j].skill &&
+                                $scope.userRoutine[i].shape &&
+                                $scope.userRoutine[i].shape === $scope.userRoutine[j].shape){
+                                    console.log("shapes");
                                     // copy new move and set tariff to 0
-                                    var newMove =  angular.copy($scope.userRoutine[i]);
-                                    newMove.tariff = 0;
-                                    // mark move as a repeated move
+                                    var newMove = angular.copy($scope.userRoutine[i]);
                                     newMove.repeatMove = true;
-                                    $scope.userRoutine[i] = newMove;
+                                    $scope.userRoutine[i] = newMove; 
+                                    
+                                } else if($scope.userRoutine[j].skill && 
+                                $scope.userRoutine[i].skill === $scope.userRoutine[j].skill  &&
+                                !$scope.userRoutine[i].shape){
+                                    console.log("no shapes");
+                                    // mark move as a repeated move
+                                    var newMove = angular.copy($scope.userRoutine[i]);
+                                    newMove.repeatMove = true;
+                                    $scope.userRoutine[i] = newMove;   
                                 }
                             }
                         }
@@ -41,24 +53,42 @@ angular
                 
                 $scope.checkMoveTariff = function (move) {
                     var sum = 0.0;
-                    if(move.tariff && move.shapeTariff){
-                        sum += move.tariff + move.shapeTariff;
-                        
-                    }else if(move.tariff){
-                        sum += move.tariff;
+                    if(move.repeatMove === undefined 
+                    || move.repeatMove === false){
+                        if(move.tariff && move.shape){
+                            sum += move.tariff;
+                            if(move.shape === 'straight' || move.shape === 'pike' ){
+                                sum += move.shape_bonus;
+                            }
+                            sum = Math.round( sum * 10) / 10;
+                        }else if(move.tariff){
+                            sum += move.tariff;
+                            sum = Math.round( sum * 10) / 10;
+                        } else if(move.tariff === 0){
+                            return 0.0;
+                        }
                     }
-                    return Math.round( sum * 10) / 10;
+                    return sum;
                 }
                 
                 function checkMoveTariff (move) {
                     var sum = 0.0;
-                    if(move.tariff && move.shapeTariff){
-                        sum += move.tariff + move.shapeTariff;
-                        
-                    }else if(move.tariff){
-                        sum += move.tariff;
+                    if(move.repeatMove === undefined 
+                    || move.repeatMove === false){
+                        if(move.tariff && move.shape){
+                            sum += move.tariff;
+                            if(move.shape === 'straight' || move.shape === 'pike' ){
+                                sum += move.shape_bonus;
+                            }
+                            sum = Math.round( sum * 10) / 10;
+                        }else if(move.tariff){
+                            sum += move.tariff;
+                            sum = Math.round( sum * 10) / 10;
+                        } else if(move.tariff === 0){
+                            return 0.0;
+                        }
                     }
-                    return Math.round( sum * 10) / 10;
+                    return sum;
                 }
 
                 function checkTariff (userRoutine) {
